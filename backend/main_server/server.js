0f38f5a11956b37bde7f982721c2248d7a20e6f1
“Yassine <Yassine.Sadek@edu.unto.it>
2025-01-12 00:52:10 +0100

    Placeholder

--- backend/main_server/server.js
+++ backend/main_server/server.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('frontend/public'));
+
+const moviesRouter = require('./routes/movies');
+const reviewsRouter = require('./routes/reviews');
+
+app.use('/movies', moviesRouter);
+app.use('/reviews', reviewsRouter);
+
 app.get('/', (req, res) => {
     res.send({ status: 'Server is running' });
 });
