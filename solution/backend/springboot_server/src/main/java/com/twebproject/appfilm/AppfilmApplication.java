172a214ac3ef7cd91f3ee22c12d600a8920acedf
“Yassine <Yassine.Sadek@edu.unto.it>
2025-01-26 22:50:49 +0100

    Initialize Spring Boot application with Gradle setup and basic configuration files

---
+++ solution/backend/springboot_server/src/main/java/com/twebproject/appfilm/AppfilmApplication.java
@@ -0,0 +1,13 @@
+package com.twebproject.appfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfilmApplication.class, args);
+	}
+
+}
