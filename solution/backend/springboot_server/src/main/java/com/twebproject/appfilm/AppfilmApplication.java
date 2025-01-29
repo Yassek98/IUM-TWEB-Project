390d6493f1c310dff93f538852538f0b7dd97786
“Yassine <Yassine.Sadek@edu.unto.it>
2025-01-29 21:43:32 +0100

    Add Swagger configuration and enable Web MVC; update ActorRepository to include custom query for name search; enhance logging settings

--- solution/backend/springboot_server/src/main/java/com/twebproject/appfilm/AppfilmApplication.java
+++ solution/backend/springboot_server/src/main/java/com/twebproject/appfilm/AppfilmApplication.java
@@ -2,8 +2,10 @@ package com.twebproject.appfilm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class AppfilmApplication {
 
 	public static void main(String[] args) {
--- solution/backend/springboot_server/src/main/java/com/twebproject/appfilm/repositories/ActorRepository.java
+++ solution/backend/springboot_server/src/main/java/com/twebproject/appfilm/repositories/ActorRepository.java
@@ -1,8 +1,12 @@
 package com.twebproject.appfilm.repositories;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.twebproject.appfilm.models.filmactors.Actor;
 import com.twebproject.appfilm.models.filmactors.ActorId;
 
@@ -18,5 +22,6 @@ public interface ActorRepository extends JpaRepository<Actor, ActorId> {
      * @param name the string to search for in actor names
      * @return a list of Actor entities with names containing the specified string
      */
-    List<Actor> findByIdName(String name);
+    @Query(value = "SELECT * FROM actors a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :name, '%'))", nativeQuery=true) // JPQL query
+    List<Actor> findByIdName(@Param("name") String name);
 }
