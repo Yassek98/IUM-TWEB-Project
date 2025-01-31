2474c54c74c762a99684fd873f57c3cd080d1a5d
Samuele Caffo <caffo.samuele@educ.di.unito.it>
2025-01-31 17:46:33 +0100

    Added chat UI and routes to test it (css and hbs files)

--- solution/frontend/client_server/app.js
+++ solution/frontend/client_server/app.js
@@ -1,27 +1,46 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
 
 const app = express();
 
-app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
+// Configura Handlebars come view engine
+app.engine('hbs', exphbs.engine({
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    defaultLayout: 'main',  // layout di default
+    partialsDir: path.join(__dirname, 'views/partials') // Directory per i partials
+}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+// Middleware
+app.use(logger('dev')); // Log di Morgan per lo sviluppo
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
+
+// Error handling
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
+    // render the error page
+    res.status(err.status || 500);
     res.render('error');
-  });
+});
 
 module.exports = app;
